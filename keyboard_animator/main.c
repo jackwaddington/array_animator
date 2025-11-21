80b0b7071cfde332f925d5047dbe41640bd70949
Jack Waddington <jack@waddington.me>
2025-11-21 13:12:13 +0200

    initial idea

---
+++ keyboard_animator/main.c
@@ -0,0 +1,45 @@
+char *["1234567890-=];
+char *["qwertyuiop[]"];
+char *["zxcvbnm,./"];
+
+typedef s_data{
+	char	*keyboard;
+	int	row;
+	int	pos;
+} t_data;
+
+int	is_key(int i, int j, t_data d)
+{
+	if(keyboard[i, j] = c)
+	{
+		d->row = i;
+		d->pos = j;
+		return 1;
+	}
+	return 0;
+}
+
+int	find_char(char c, char *keyboard, t_data d)
+{
+	int i = 0; // row
+	int j = 0; // pos
+
+	while(i < 3)
+	{
+		while(j < strlen(keyboard[i])
+		{
+			if(is_key(i, j, d)
+				return;
+			else
+				i++;
+			j++;
+		}
+
+		if (keyboard[i, j] == c){
+			d->row = i;
+			d->pos = j;
+			i = 0;
+			return;
+		}
+	}
+}
